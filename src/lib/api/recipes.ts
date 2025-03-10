8453a9cdf50e2e4fca1622e8d7a701ffe92d7755
kruegerhash <L.work2010@outlook.de>
2025-03-10 13:42:35 +0100

    add getChats and deleteChat functionality

--- src/lib/api/recipes.ts
+++ src/lib/api/recipes.ts
@@ -41,3 +41,48 @@ export const getMessages = async (chatId: string) => {
         throw new Error("Error getting messages");
     }
 };
+
+export const getChats = async () => {
+    try {
+        // track execution time
+        console.time("getChats");
+        const endpoint = `${config.pythonEndpoint}/llm/get_chats`;
+        const response = await fetch(endpoint, {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
+        const result = await response.json();
+
+        const restructuedResult = result.map((chat: any) => {
+            return {
+                link: `/generate-recipe/${chat.chat_id}`,
+                title: chat.title,
+            };
+        });
+        console.timeEnd("getChats");
+        console.log("getChats", restructuedResult);
+        return restructuedResult;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error getting chats");
+    }
+};
+
+export const deleteChat = async (chadId: string) => {
+    try {
+        const endpoint = `${config.pythonEndpoint}/llm/delete_chat/${chadId}`;
+        const response = await fetch(endpoint, {
+            method: "delete",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return response;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error deleting chat");
+    }
+};

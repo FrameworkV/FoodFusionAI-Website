b717111506027fb6b8965113cf09460c1ce39589
kruegerhash <L.work2010@outlook.de>
2025-06-25 21:54:18 +0200

    add optional loader to pageswrapper

--- src/_root/components/wrapper.tsx
+++ src/_root/components/wrapper.tsx
@@ -1,7 +1,9 @@
+import Loader from "@/components/loader"
 import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
 
-const PagesWrapper = ({children, className=""}:{children: ReactNode, className?:string}) => {
+const PagesWrapper = ({children, className="", isLoading=false}:{children: ReactNode, className?:string,isLoading?:boolean}) => {
+  if(isLoading) return <Loader/>
   return (
     <div className={cn("px-16 py-8 w-full",className)}>
         {children}

fe3798e86989ccd1afa6c306926d387c4e92cf96
kruegerhash <L.work2010@outlook.de>
2025-06-25 20:09:25 +0200

    fix pageswrapper, add custom className property

--- src/_root/components/wrapper.tsx
+++ src/_root/components/wrapper.tsx
@@ -1,8 +1,9 @@
+import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
 
-const PagesWrapper = ({children}:{children: ReactNode}) => {
+const PagesWrapper = ({children, className=""}:{children: ReactNode, className?:string}) => {
   return (
-    <div className="px-16 py-8 w-full">
+    <div className={cn("px-16 py-8 w-full",className)}>
         {children}
     </div>
   )
